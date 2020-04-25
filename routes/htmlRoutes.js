d9711e567ee7a454d83850d724edf0b5d7fe29b0
Erika Vigliucci <evigliucci@gmail.com>
2020-04-25 13:19:33 -0400

    add all files

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,32 @@
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+    // HTML GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases the user is shown an HTML page of content
+    // ---------------------------------------------------------------------------
+
+    //app.get for /tables
+    app.get("/tables", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/tables.html"));
+    });
+    //app.get for /reserve
+    app.get("/reserve", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/reserve.html"));
+    });
+
+    //app.get for home
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
+};
